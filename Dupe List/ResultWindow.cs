e6effd41601df2e671054b3584c314959209b556
Maxim Rubis <siburny@gmail.com>
2019-06-29 00:03:14 -0400

    Delete context menu; add ability to select Lower Priority files

--- Dupe List/ResultWindow.cs
+++ Dupe List/ResultWindow.cs
@@ -20,80 +20,10 @@ namespace DupeList
         {
             InitializeComponent();
 
-            MenuItem[] SelectSubitems = new MenuItem[2];
-            SelectSubitems[0] = new MenuItem();
-            SelectSubitems[0].Text = "First Item in Group";
-            SelectSubitems[0].Click += new EventHandler(ResultWindow_Click);
-            SelectSubitems[1] = new MenuItem();
-            SelectSubitems[1].Text = "Second Item in Group";
-            SelectSubitems[1].Click += new EventHandler(ResultWindow2_Click);
-
-            cm.MenuItems.Add(new MenuItem("Select", SelectSubitems));
-
-            cm.MenuItems.Add(new MenuItem("-"));
-            cm.MenuItems.Add(new MenuItem("Select All", SelectAll_Click));
-            cm.MenuItems.Add(new MenuItem("Select None", SelectNone_Click));
-            cm.MenuItems.Add(new MenuItem("Select Inverse", SelectInverse_Click));
-            cm.MenuItems.Add(new MenuItem("-"));
-            cm.MenuItems.Add(new MenuItem("Delete Selected", DeleteSelected_Click));
-
-            duplicateList.ContextMenu = cm;
-
             new Thread((ThreadStart)delegate()
             {
                 BeginProcessing(duplicateResults);
             }).Start();
-        }
-
-        void DeleteSelected_Click(object sender, EventArgs e)
-        {
-            new Thread((ThreadStart)delegate()
-            {
-                _deleteSelected();
-            }).Start();
-        }
-
-        void SelectAll_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewItem i in duplicateList.Items)
-            {
-                i.Selected = true;
-            }
-        }
-
-        void SelectNone_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewItem i in duplicateList.Items)
-            {
-                i.Selected = false;
-            }
-        }
-
-        void SelectInverse_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewItem i in duplicateList.Items)
-            {
-                i.Selected = !i.Selected;
-            }
-        }
-
-        void ResultWindow2_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewGroup g in duplicateList.Groups)
-            {
-                if (g.Items.Count > 1)
-                {
-                    g.Items[1].Selected = true;
-                }
-            }
-        }
-
-        void ResultWindow_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewGroup g in duplicateList.Groups)
-            {
-                g.Items[0].Selected = true;
-            }
         }
 
         private void BeginProcessing(Hashtable duplicateResults)
@@ -206,6 +136,25 @@ namespace DupeList
             }
         }
 
+        private void bSelLowerPriority_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem i in duplicateList.Items)
+            {
+                i.Checked = ExcludeFiles.IsLowerPriority(i.Text); 
+            }
+
+            foreach (ListViewGroup g in duplicateList.Groups)
+            {
+                if(g.Items.OfType<ListViewItem>().All(x => !x.Checked))
+                {
+                    for(int index  = 0; index < g.Items.Count; index ++)
+                    {
+                        (g.Items[index] as ListViewItem).Checked = index != 0;
+                    }
+                }
+            }
+        }
+
         private void bDelSelected_Click(object sender, EventArgs e)
         {
             new Thread((ThreadStart)delegate()
@@ -233,75 +182,6 @@ namespace DupeList
                     foreach (ListViewItem i in duplicateList.Items)
                     {
                         if (i.Checked)
-                        {
-                            try
-                            {
-                                FileInfo f = new FileInfo(i.Text);
-                                f.Delete();
-                                toRemove.Add(i);
-                            }
-                            catch
-                            {
-                                // Delete failed. Skip the file.
-                            }
-                        }
-                        total++;
-                        progress.UpdateProgress(total);
-                    }
-                });
-
-                DoInvoke((MethodInvoker)delegate()
-                {
-                    progress.Close();
-                });
-
-                ProcessProgress cleanup = new ProcessProgress(toRemove.Count, "Cleaning up");
-                int count = 0;
-                DoInvoke((MethodInvoker)delegate()
-                {
-                    cleanup.Show(this);
-
-                    // Remove deleted files
-                    duplicateList.BeginUpdate();
-                    foreach (ListViewItem i in toRemove)
-                    {
-                        duplicateList.Items.Remove(i);
-                        cleanup.UpdateProgress(count++);
-                    }
-                    duplicateList.EndUpdate();
-                    cleanup.Close();
-                });
-
-                if (toRemove.Count != count)
-                {
-                    MessageBox.Show("Some files could not be deleted. These items were not removed from the list.", "Partial File Deletion", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-                else
-                {
-                    MessageBox.Show("All selected files have been deleted.", "All Files Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-            }
-        }
-
-        private void _deleteSelected()
-        {
-            if (MessageBox.Show("WARNING: This will permanently delete all selected files in this list. This action cannot be undone.\r\n\r\nAre you sure you want to delete the selected files?", "Confirm Deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
-            {
-                ProcessProgress progress = new ProcessProgress(duplicateList.Items.Count, "Deleting");
-                DoInvoke((MethodInvoker)delegate()
-                {
-                    progress.Show(this);
-                });
-
-                List<ListViewItem> toRemove = new List<ListViewItem>();
-
-                int total = 0;
-
-                DoInvoke((MethodInvoker)delegate()
-                {
-                    foreach (ListViewItem i in duplicateList.Items)
-                    {
-                        if (i.Selected)
                         {
                             try
                             {
